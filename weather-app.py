4c49386ef881d92dded0337efcdf4dec2dc83870
Justice-Owens <justicehawke@gmail.com>
2024-01-31 13:02:29 -0500

    Added functionality to check another city after initial search

--- weather-app.py
+++ weather-app.py
@@ -39,15 +39,28 @@ def kelvin_to_f(kelvin):
 
 
 if __name__ == "__main__":
+
     api_key = input("Enter your OpenWeather Map API key: ")
-    city = input("Enter the city name: ")
-    weather = get_weather(api_key, city)
 
-    temp_in_k = weather['main']['temp']
-    temp_in_f = kelvin_to_f(temp_in_k)
+    while True:
+        city = input("Enter the city name: ")
 
-    if weather:
-        print(f"Weather in {city}: {weather['weather'][0]['description']}")
-        print(f"Temperature: {int(temp_in_f)} °F")
-        print(f"Humidity: {weather['main']['humidity']}")
-        print(f"Feels Like: {int(kelvin_to_f(weather['main']['feels_like']))} °F")
+        while not city or city.isdigit():
+            print("Entry was not valid. Please enter a valid city name")
+            city = input("Please enter the city name: ")
+
+        weather = get_weather(api_key, city)
+
+        temp_in_k = weather['main']['temp']
+        temp_in_f = kelvin_to_f(temp_in_k)
+
+        if weather:
+            print(f"Weather in {city}: {weather['weather'][0]['description']}")
+            print(f"Temperature: {int(temp_in_f)} °F")
+            print(f"Humidity: {weather['main']['humidity']}")
+            print(f"Feels Like: {int(kelvin_to_f(weather['main']['feels_like']))} °F")
+
+        ask = input("Would you like to get the weather for another city? Y/N ")
+        if ask.lower() == 'n':
+            print("Exiting application...")
+            break
